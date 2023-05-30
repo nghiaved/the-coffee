5f27f0bd8c500483ac3e2d31c598a24c19aa40eb
nghiaved <nghiaved@gmail.com>
2023-05-30 12:42:41 +0700

    product page

--- client/src/app/pages/Products/ProductList.js
+++ client/src/app/pages/Products/ProductList.js
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { apiProductRead } from '../../../services'
+
 function ProductList() {
+    const [allProducts, setAllProducts] = useState([])
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        const res = await apiProductRead()
+        setAllProducts(res.allProducts)
+    }
+
     return (
         <div className='product-list-wrapper'>
-            ProductList component
+            {allProducts.map((item, index) => {
+                return <div key={index} className='product-item'>
+                    <img src={item.image} alt='' />
+                    <div className='bulletin'>
+                        <div className='name'>
+                            {item.name}
+                        </div>
+                        <div className='price'>
+                            {item.price}
+                        </div>
+                    </div>
+                </div>
+            })}
         </div>
     );
 }
